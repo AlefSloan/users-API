a6a308114504d3902e77dd29bce687e878a7425d
alefsloan <alef_sloan@hotmail.com>
2023-02-07 14:50:45 -0300

    feat: create CreateUser useCase

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,15 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const existUser = this.usersRepository.findByEmail(email);
+
+    if (existUser) {
+      throw new Error("Usuário já cadastrado");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
   }
 }
 
